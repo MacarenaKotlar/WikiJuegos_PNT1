17501b853b6c7ccb2478a9bcfaf1e21a0e9bccc7
Macarena Kotlar <maca.esc.k@gmail.com>
2025-11-10 22:32:32 -0300

    creación de clase Juego

---
+++ WikiJuegos_PNT1/Models/Juego.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class Juego
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+        public string Desarrollador { get; set; }
+
+        [Display(Name = "Fecha de lanzamiento")]
+        public DateTime FechaLanzamiento { get; set; }
+
+        [EnumDataType(typeof(Genero))]
+        public Genero Genero { get; set; }
+    }
+}
+
+
