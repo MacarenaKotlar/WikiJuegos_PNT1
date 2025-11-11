06a9cd01213bccddff68ac8b90a9abfc2879fee5
disco probador <disco probador@DESKTOP-5AFU4IF>
2025-11-10 21:31:38 -0300

    Agregar archivos de proyecto.

---
+++ WikiJuegos_PNT1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WikiJuegos_PNT1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
