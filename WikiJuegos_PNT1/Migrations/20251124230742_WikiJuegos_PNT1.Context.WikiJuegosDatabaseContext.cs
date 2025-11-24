99cd8d2a64c3a3fd729a8a5b3c4ebb7463e1513b
Maca <maca.esc.k@gmail.com>
2025-11-24 20:17:56 -0300

    agregar migraciones

---
+++ WikiJuegos_PNT1/Migrations/20251124230742_WikiJuegos_PNT1.Context.WikiJuegosDatabaseContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WikiJuegos_PNT1.Migrations
+{
+    /// <inheritdoc />
+    public partial class WikiJuegos_PNT1ContextWikiJuegosDatabaseContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Juegos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Desarrollador = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaLanzamiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Genero = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Juegos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Juegos");
+        }
+    }
+}
