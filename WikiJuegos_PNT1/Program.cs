f6744117ced0fb5b871eb146cde01d346f445cc4
Maca <maca.esc.k@gmail.com>
2025-11-24 01:24:48 -0300

    configurar Program.cs

--- WikiJuegos_PNT1/Program.cs
+++ WikiJuegos_PNT1/Program.cs
@@ -1,7 +1,35 @@
+using Azure.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WikiJuegos_PNT1.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential
+    //cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+builder.Services.AddDbContext<WikiJuegosDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration["ConnectionString:WikiJuegosDBConnection"
+]));
 
 var app = builder.Build();
 
