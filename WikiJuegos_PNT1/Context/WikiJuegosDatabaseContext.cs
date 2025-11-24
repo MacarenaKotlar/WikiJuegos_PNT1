3c2d0e6c3f063edeb0ff252457e4270cf47e332a
Maca <maca.esc.k@gmail.com>
2025-11-23 21:35:27 -0300

    creación carpeta Context, creación clase WikiJuegosDatabaseContext.cs

---
+++ WikiJuegos_PNT1/Context/WikiJuegosDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WikiJuegos_PNT1.Models;
+
+namespace WikiJuegos_PNT1.Context
+{
+    public class WikiJuegosDatabaseContext : DbContext
+    {
+        public
+        WikiJuegosDatabaseContext(DbContextOptions<WikiJuegosDatabaseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Juego> Juegos { get; set; }
+    }
+}
