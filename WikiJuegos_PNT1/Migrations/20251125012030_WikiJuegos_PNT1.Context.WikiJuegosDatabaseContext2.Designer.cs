e7cdbb17c1b05fb6677ed6370f2c52ec012cd8f0
Maca <maca.esc.k@gmail.com>
2025-11-25 01:35:19 -0300

    migraciones de Juego y Usuario

--- WikiJuegos_PNT1/Migrations/20251125012030_WikiJuegos_PNT1.Context.WikiJuegosDatabaseContext2.Designer.cs
+++ WikiJuegos_PNT1/Migrations/20251125012030_WikiJuegos_PNT1.Context.WikiJuegosDatabaseContext2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WikiJuegos_PNT1.Context;
 
@@ -11,9 +12,11 @@ using WikiJuegos_PNT1.Context;
 namespace WikiJuegos_PNT1.Migrations
 {
     [DbContext(typeof(WikiJuegosDatabaseContext))]
-    partial class WikiJuegosDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20251125012030_WikiJuegos_PNT1.Context.WikiJuegosDatabaseContext2")]
+    partial class WikiJuegos_PNT1ContextWikiJuegosDatabaseContext2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
