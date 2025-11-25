85398cae2bd8fdf90b778a0aa0087ebebb9af257
Maca <maca.esc.k@gmail.com>
2025-11-25 01:37:26 -0300

    creación de modelos de vista RegistroVM y LoginVM para la verificación de credenciales

---
+++ WikiJuegos_PNT1/ViewModels/LoginVM.cs
@@ -0,0 +1,8 @@
+﻿namespace WikiJuegos_PNT1.ViewModels
+{
+    public class LoginVM
+    {
+        public required string Nombre { get; set; }
+        public required string Contra { get; set; }
+    }
+}
---
+++ WikiJuegos_PNT1/ViewModels/RegistroVM.cs
@@ -0,0 +1,9 @@
+﻿namespace WikiJuegos_PNT1.ViewModels
+{
+    public class RegistroVM
+    {
+        public required string Nombre { get; set; }
+        public required string Contra { get; set; }
+        public required string ConfirmarContra { get; set; }
+    }
+}
