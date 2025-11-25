cbd466a16cbfca164f26eace882035f1d6fc4374
Maca <maca.esc.k@gmail.com>
2025-11-25 01:34:34 -0300

    creación clase Usuario

---
+++ WikiJuegos_PNT1/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace WikiJuegos_PNT1.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required string Contra { get; set; }
+    }
+}
