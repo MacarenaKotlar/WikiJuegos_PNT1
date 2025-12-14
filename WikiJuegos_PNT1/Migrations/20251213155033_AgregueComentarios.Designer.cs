0a398de5f4c0977d0a74c5b783f69c56f6fbeacf
GABRIEL LUIS GARCIA <gabrieldfgwe@gmail.com>
2025-12-13 21:31:31 -0300

    Agrego tabla Comentarios y validaciones de Juego

--- WikiJuegos_PNT1/Migrations/20251213155033_AgregueComentarios.Designer.cs
+++ WikiJuegos_PNT1/Migrations/20251213155033_AgregueComentarios.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WikiJuegos_PNT1.Context;
 
@@ -11,9 +12,11 @@ using WikiJuegos_PNT1.Context;
 namespace WikiJuegos_PNT1.Migrations
 {
     [DbContext(typeof(WikiJuegosDatabaseContext))]
-    partial class WikiJuegosDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20251213155033_AgregueComentarios")]
+    partial class AgregueComentarios
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WikiJuegos_PNT1/Migrations/20251213155033_AgregueComentarios.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WikiJuegos_PNT1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregueComentarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Juegos",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Juegos",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Desarrollador",
+                table: "Juegos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Comentarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Texto = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Puntaje = table.Column<int>(type: "int", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UsuarioNombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    JuegoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comentarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comentarios_Juegos_JuegoId",
+                        column: x => x.JuegoId,
+                        principalTable: "Juegos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comentarios_JuegoId",
+                table: "Comentarios",
+                column: "JuegoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comentarios");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Juegos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Juegos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Desarrollador",
+                table: "Juegos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
